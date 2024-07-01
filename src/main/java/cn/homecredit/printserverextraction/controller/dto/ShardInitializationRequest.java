2bd36923f31d08e990995b337980d5f5825930f3
Jay.Wang1 <jay.wang1@homecreditcfc.cn>
2024-07-01 13:49:11 +0800

    [RDCN-3585]INIT

--- src/main/java/cn/homecredit/printserverextraction/controller/dto/ShardInitializationRequest.java
+++ src/main/java/cn/homecredit/printserverextraction/controller/dto/ShardInitializationRequest.java
@@ -12,7 +12,7 @@ public class ShardInitializationRequest {
     //每个分片处理的记录数，即每个线程每次拿走的contract数据记录数，这个数不能太大，否则全量查询MySQL中数据load到jvm中会导致内存溢出
     //1个亿的数据，则会产生10w的分片
     @NotNull(message = "shardSize is required")
-    @Min(value = 100, message = "shardSize must be greater than 100")
+    @Min(value = 10, message = "shardSize must be greater than 100")
     private int shardSize=1000;
 
     // Getters and Setters
